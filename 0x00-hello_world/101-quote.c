3fd6c70f697960ad0ce809c3b6ef19cb5926ad82
igueelijah1 <igueelijah1@gmail.com>
2023-07-24 06:27:56 -0700

    formhello

--- 0x00-hello_world/101-quote.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main() {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59); // 59 is the length of the message
-
-    return 1;
-}
-
